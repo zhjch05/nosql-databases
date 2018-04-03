523e136eb0397d6fc6ceb004fb612165f1f3bb51
Jincheng Zhang <zhjch05@live.com>
2018-04-02 21:01:38 -0400

    update date

--- redis/homework_1.rb
+++ redis/homework_1.rb
@@ -3,7 +3,7 @@ require 'httparty'
 
 # Set up the url and send a GET request to it. The base url is:
 # "https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo"
-response = HTTParty.get('https://api.nasa.gov/planetary/apod?api_key=IJRWOz0xpNNtJT40JUKsoCq9wtcbyhIRhzMpzo0M')
+response = HTTParty.get('https://api.nasa.gov/planetary/apod?api_key=IJRWOz0xpNNtJT40JUKsoCq9wtcbyhIRhzMpzo0M&', date: "2018-01-22")
 
 # Make the request and print out the "url" key in the response, which is the image url
 print(response["url"])
