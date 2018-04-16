77b1d8f8a011ad8d54619fca47818c4a1de4d05b
Jincheng Zhang <zhjch05@live.com>
2018-04-16 19:10:45 -0400

    part D done

--- mongodb/homework_3.rb
+++ mongodb/homework_3.rb
@@ -15,16 +15,17 @@ movies = client[:movies]
 
 #part A
 result = movies.update_many({"rated" => "NOT RATED"}, {"$set" => {"rated" => "Pending rating"}})
-puts result.modified_count
+#puts result.modified_count
 
-#should return nil
+#test: should return nil
 #pp movies.find({rated: "NOT RATED"}).limit(1).first
 
 #Drama example
 #pp movies.find({"genres" => ["Drama"]}).limit(1).first
 
+#part B
 #insert a record from info of imdb
-unless movies.find({:title => "Game of Thrones"}).limit(1).first.nil?
+if movies.find({:title => "Game of Thrones"}).limit(1).first.nil?
   result = movies.insert_one({
     :title => "Game of Thrones",
     :year => 2011,
@@ -33,7 +34,34 @@ unless movies.find({:title => "Game of Thrones"}).limit(1).first.nil?
     :directors => ["David Benioff", "D.B. Weiss"],
     :imdb => {"rating" => 9.5, "votes" => 1310062, "id" => 944947},
   })
-  puts result.modified_count
+  puts result.n
 end
 
-pp movies.find({:title => "Game of Thrones"}).limit(1).first
+# test insertion
+#pp movies.find({:title => "Game of Thrones"}).limit(1).first
+
+#part C
+aggregation = movies.aggregate([
+  {"$match" => {"genres" => "Drama"}},
+  {"$unwind" => "$genres"},
+  {"$group" => {"_id" => "$genres", "count" => {"$sum" => 1}}},
+])
+aggregation.each do |doc|
+  if doc["_id"] == "Drama"
+    puts doc
+  end
+end
+
+#part D
+#test a record
+#pp movies.find({"countries" => {"$regex" => ".*[Cc]hina.*"}}).limit(1).first
+aggregation = movies.aggregate([
+  {"$match" => {"countries" => "China", "rated" => "Pending rating"}},
+  {"$unwind" => "$countries"},
+  {"$group" => {"_id" => {"country" => "$countries", "rating" => "Pending rating"}, "count" => {"$sum" => 1}}},
+])
+aggregation.each do |doc|
+  if doc["_id"]["country"] == "China"
+    puts doc
+  end
+end
